1e7f8f20589d18e28071f55c6eda2b4260e1f505
Nick Golding <goldingn35@gmail.com>
2016-11-23 16:38:49 +1030

    add a load of functions

--- R/functions.R
+++ R/functions.R
@@ -1,15 +1,59 @@
 #' @name greta-functions
 #'
-#' @title functions for greta nodes
+#' @title R functions that work for greta nodes
 #'
-#' @description This is a list of currently implemented functions to transform
-#'  greta nodes. More will follow.
+#' @description This is a list of functions in base R that are currently
+#'   implemented to transform greta nodes.
 #'
 #' @section Usage: \preformatted{
+#'
+#'  # logarithms and exponentials
 #'  log(x)
 #'  exp(x)
+#'  log1p(x)
+#'  expm1(x)
+#'
+#'  # miscellaneous mathematics
+#'  abs(x)
+#'  sqrt(x)
+#'  sign(x)
+#'
+#'  # rounding of numbers
+#'  ceiling(x)
+#'  floor(x)
+#'  round(x, digits = 0)
+#'
+#'  # trigonometry
+#'  cos(x)
+#'  sin(x)
+#'  tan(x)
+#'  acos(x)
+#'  asin(x)
+#'  atan(x)
+#'
+#'  # special mathematical functions
+#'  lgamma(x)
+#'  digamma(x)
+#'
+#'  # matrix operations
 #'  t(x)
+#'  chol(x, ...)
 #'  }
+#'
+#' @details TensorFlow only enables rounding to integers, so \code{round()} will
+#'   error is set to anything other than \code{0}. Any additional arguments to
+#'   \code{chol()} will be ignored, see the TensorFlow documentation for details
+#'   of the cholesky routine.
+#'
+#' @examples
+#' x = observed(matrix(1:9, nrow = 3, ncol = 3))
+#' a = log(exp(x))
+#' b = log1p(expm1(x))
+#' c = sign(x - 5)
+#' d = abs(x - 5)
+#'
+#' z = t(a)
+#'
 NULL
 
 #' @export
@@ -20,6 +64,89 @@ NULL
 #' @export
 `exp.node` <- function (e1) {
   op("tf$exp", e1)
+}
+
+#' @export
+`log1p.node` <- function (e1) {
+  log(1 + e1)
+}
+
+#' @export
+`expm1.node` <- function (e1) {
+  exp(e1) - 1
+}
+
+#' @export
+`abs.node` <- function (e1) {
+  op("tf$abs", e1)
+}
+
+#' @export
+`sqrt.node` <- function (e1) {
+  op("tf$sqrt", e1)
+}
+
+#' @export
+`sign.node` <- function (e1) {
+  op("tf$sign", e1)
+}
+
+#' @export
+`ceiling.node` <- function (e1) {
+  op("tf$ceil", e1)
+}
+
+#' @export
+`floor.node` <- function (e1) {
+  op("tf$floor", e1)
+}
+
+#' @export
+`round.node` <- function (e1, digits = 0) {
+  if (digits != 0)
+    stop("TensorFlow round only supports rounding to integers")
+  op("tf$round", e1)
+}
+
+# trigonometry functions
+#' @export
+`cos.node` <- function (e1) {
+  op("tf$cos", e1)
+}
+
+#' @export
+`sin.node` <- function (e1) {
+  op("tf$sin", e1)
+}
+
+#' @export
+`tan.node` <- function (e1) {
+  op("tf$tan", e1)
+}
+
+#' @export
+`acos.node` <- function (e1) {
+  op("tf$acos", e1)
+}
+
+#' @export
+`asin.node` <- function (e1) {
+  op("tf$asin", e1)
+}
+
+#' @export
+`atan.node` <- function (e1) {
+  op("tf$atan", e1)
+}
+
+#' @export
+`lgamma.node` <- function (e1) {
+  op("tf$lgamma", e1)
+}
+
+#' @export
+`digamma.node` <- function (e1) {
+  op("tf$digamma", e1)
 }
 
 #' @export
@@ -34,4 +161,20 @@ NULL
   }
 
   op("tf$transpose", e1, dimfun = dimfun)
+}
+
+#' @export
+`chol.node` <- function (e1, ...) {
+
+  if (!identical(list(), list(...)))
+    warning ('chol() options are ignored by TensorFlow')
+
+  dimfun <- function (node_list) {
+    dim <- node_list[[1]]$dim
+    if ( !(length(dim) == 2 && dim[1] == dim[2]) )
+      stop ('only 2D, square can Cholesky decomposed')
+    dim
+  }
+
+  op("tf$cholesky", e1, dimfun = dimfun)
 }
