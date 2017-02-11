c9c443aafe08300ffeb33260a580d6a70d76e75d
Nick Golding <goldingn35@gmail.com>
2017-02-11 22:29:35 +1100

    add replace syntax for nodes

--- R/extract_replace.R
+++ R/extract_replace.R
@@ -14,6 +14,70 @@ tf_extract <- function (x, nelem, tf_index, dims_out) {
 
 }
 
+# using tf$concat, update the elements of a tensor `ref`, putting the new
+# values, a tensor `updates` at the elements given by the R vector `index` (in
+# 0-indexing)
+recombine <- function (ref, index, updates) {
+
+  # vector denoting whether an element is being updated
+  nelem <- ref$get_shape()$as_list()
+  replaced <- rep(0, nelem)
+  replaced[index + 1] <- seq_along(index)
+  runs <- rle(replaced)
+
+  # number of blocks to concatenate
+  nblock <- length(runs$lengths)
+
+  # start location (R-style) for each block in the original object
+  starts_old <- cumsum(c(0, runs$lengths[-nblock])) + 1
+
+  # list of non-updated values
+  keep_idx <- which(runs$values == 0)
+  keep_list <- lapply(keep_idx, function (i) {
+    idx <- starts_old[i] + 0:(runs$lengths[i] - 1) - 1
+    tf$reshape(ref[idx], shape(length(idx), 1))
+  })
+
+  run_id <- runs$values[runs$values != 0]
+  update_idx <- match(run_id, runs$values)
+  # get them in order increasing order
+  update_list <- lapply(run_id, function (i) {
+    tf$reshape(updates[i - 1], shape(1, 1))
+  })
+
+  # combine them
+  full_list <- list()
+  full_list[keep_idx] <- keep_list
+  full_list[update_idx] <- update_list
+
+  # concatenate the vectors
+  result <- tf$concat(tf$constant(0L),
+                      full_list)
+
+  # rotate it
+  result <- tf$reshape(result, shape(result$get_shape()$as_list()[1]))
+  result
+
+}
+
+# replace elements in a tensor with another tensor
+tf_replace <- function (x, value, index, dims) {
+
+  # flatten original tensor and new values
+  nelem <- prod(dims)
+  x_flat <- tf$reshape(x, shape(nelem))
+  value_flat <- tf$reshape(value, shape(length(index)))
+
+  # update the values into a new tensor
+  result_flat <- recombine(ref = x_flat,
+                          index = index,
+                          updates = value_flat)
+
+  # reshape the result
+  result <- tf$reshape(result_flat, to_shape(dims))
+  result
+
+}
 
 
 # extract syntax for nodes
@@ -53,5 +117,38 @@ tf_extract <- function (x, nelem, tf_index, dims_out) {
      operation_args = list(nelem = nelem,
                            tf_index = tf_index,
                            dims_out = dims_out))
+
+}
+
+
+# extract syntax for nodes
+#' @export
+`[<-.node` <- function(x, ..., value) {
+
+  # store the full call to mimic on a dummy array, plus the array's dimensions
+  call <- sys.call()
+  dims <- x$dim
+
+  # create a dummy array containing the order of elements Python-style
+  dummy <- dummy(dims)
+
+  # subset the dummy array using the original subsetting call
+  call[[1]] <- `[`
+  call[[2]] <- dummy
+  call$value <- NULL
+
+  index <- as.vector(eval(call))
+
+  # function to return dimensions of output
+  dimfun <- function (node_list)
+    dims
+
+  # create operation node, passing call and dims as additional arguments
+  op('tf_replace',
+     x,
+     value,
+     dimfun = dimfun,
+     operation_args = list(index = index,
+                           dims = dims))
 
 }
