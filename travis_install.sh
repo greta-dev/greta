58f1753cfb815f28a2783291a7e2a548454edb24
Nick Golding <goldingn35@gmail.com>
2017-04-29 16:24:17 +1000

    try pip travis install

--- travis_install.sh
+++ travis_install.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
+set -e
 
-sudo apt-get update
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
+# Python dependencies
+pip install --upgrade pip
+pip install numpy
 
-# Useful for debugging any issues with conda
-conda info -a
-
-pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.0.0-cp35-cp35m-linux_x86_64.whl
-python setup.py install
+# tensorflow
+pip install tensorflow 
