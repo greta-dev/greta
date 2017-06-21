651a9c2ef5d0093d74de26b3437d413a66a5698f
Nick Golding <goldingn35@gmail.com>
2017-06-21 10:23:03 +1000

    build pdf vignettes with rmarkdown

---
+++ vignettes/build_vignettes.R
@@ -0,0 +1,3 @@
+rmarkdown::render('vignettes/get_started.Rmd', output_format = 'all')
+rmarkdown::render('vignettes/example_models.Rmd', output_format = 'all')
+rmarkdown::render('vignettes/technical_details.Rmd', output_format = 'all')
