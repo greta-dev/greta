bcb52a61c33163d7225f751c5d0410a53151e70a
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-07-06 21:42:15 +0200

    Tweaked some files

---
+++ vignettes/example_models.R
@@ -0,0 +1,6 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(comment = NA,
+                      eval = greta:::check_tf_version("message"),
+                      cache = TRUE)
+library (greta)
+
