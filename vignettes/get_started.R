4ddfe859f90924b180e70806977913731e54fb7a
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-07-15 19:22:20 +0200

    Restructed some examples and added regularizing prios

--- vignettes/get_started.R
+++ vignettes/get_started.R
@@ -1,5 +1,3 @@
-## ----setup, include = FALSE----------------------------------------------
-
 knitr::opts_chunk$set(echo = TRUE,
                       eval = greta:::check_tf_version("message"),
                       cache = TRUE,
