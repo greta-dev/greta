c8b72acdc2676031d79fbba92bdcde48fa56e72b
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2018-12-07 10:40:14 +0000

    add space

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -90,9 +90,7 @@ test_that("kronecker works with greta/base array arguments", {
 
     compare_op(base_out, grab(greta_out1))
     compare_op(base_out, grab(greta_out2))
-
-
-
+    
 })
 
 test_that("aperm works as expected", {
