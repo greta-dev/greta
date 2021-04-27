053f555fcdae0595f2f8d825a328d5644e64e65b
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-04-27 15:24:59 +0800

    adds details section on LINPACK argument in overloaded to notify users an arg is defunct + silently ignored.

--- R/overloaded.R
+++ R/overloaded.R
@@ -13,5 +13,10 @@
 #'   x,y,size,LINPACK,V,na.rm,dims,MARGIN,STATS,FUN,check.margin,\dots,r,k,upper.tri,transpose,l,X,INDEX,symmetric,only.values,EISPACK,x1,x2,compact,along,rev.along,new.names,force.array,make.names,use.anon.names,use.first.dimnames,hier.names,use.dnns,nrow,ncol
 #'    arguments as in original documentation
 #'
+#' @details
+#'   Note that, as in R, the LINPACK argument is defunct and silently ignored.
+#'   The argument is only included for compatibility with the base functions
+#'   that call it.
+#'
 NULL
 # nolint end
