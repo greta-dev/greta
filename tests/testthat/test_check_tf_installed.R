0427738d3163e233b7a29180758cf7652f91761f
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-04-29 13:08:21 +0800

    Make tests a little bit more descriptive

--- tests/testthat/test_check_tf_installed.R
+++ tests/testthat/test_check_tf_installed.R
@@ -1,11 +1,10 @@
-test_that("Config/reticulate: option works", {
+test_that("Config/reticulate: has installed python and tf and tfp", {
   expect_true(have_conda())
   expect_true(have_python())
   expect_true(have_tf())
   expect_true(have_tfp())
 })
 
-test_that("tensorflow and tf probability versions are 1.14 and 0.7.0", {
-  expect_equal(tf$`__version__`, "1.14.0")
-  expect_equal(tfp$`__version__`, "0.7.0")
+test_that("tensorflow version is 1.14", {
+  expect_equal(tensorflow::tf_version(), "1.14")
 })
