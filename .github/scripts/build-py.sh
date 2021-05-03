2222d7a078d5b890d656cea43fa816738bf68d2c
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-05-03 13:09:51 +0800

    remove scripts as they are not needed

--- .github/scripts/build-py.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python -m pip install --upgrade pip
-pip install --user numpy
-pip install --user scipy
-pip install --user 'tensorflow==1.14'
-pip install --user 'tensorflow-probability==0.7.0'
