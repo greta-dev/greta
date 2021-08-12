c33fe514a190cdb170306af7402e3e2c1fd8f8fd
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-08-12 14:28:33 +0800

    don't use snapshot testing for the install_greta_deps as it is too sensitive to other machines

--- tests/testthat/test_install_greta_deps.R
+++ tests/testthat/test_install_greta_deps.R
@@ -1,6 +1,6 @@
 test_that("install_greta_deps errors appropriately", {
-  expect_snapshot(
-    error = TRUE,
-    install_greta_deps(timeout = 0.001)
+  expect_error(
+    object = install_greta_deps(timeout = 0.001),
+    regexp = "^Stopping as installation of"
   )
 })
