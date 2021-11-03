d6978e8b0334b7b56d979872caf98f09e55c6654
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-11-03 17:48:03 +0800

    create zzz.R as a home for the .onLoad functions to live.
    as seen in dplyr, e.g., https://github.com/tidyverse/dplyr/blob/434caa2db5c1f48b9f0becc9295e547c1072260b/R/zzz.r

---
+++ R/zzz.R
@@ -0,0 +1,31 @@
+# load tf probability
+tfp <- reticulate::import("tensorflow_probability", delay_load = TRUE)
+
+# crate the node list object whenever the package is loaded
+.onLoad <- function(libname, pkgname) { # nolint
+
+  # unset reticulate python environment, for more details, see:
+  # https://github.com/greta-dev/greta/issues/444
+  Sys.unsetenv("RETICULATE_PYTHON")
+
+  if (have_greta_conda_env()) {
+    use_greta_conda_env()
+  }
+
+  # silence TF's CPU instructions message
+  Sys.setenv(TF_CPP_MIN_LOG_LEVEL = 2)
+
+  # silence messages about deprecation etc.
+  # disable_tensorflow_logging()
+
+  # warn if TF version is bad
+  # check_tf_version("startup")
+
+  # switch back to 0-based extraction in tensorflow, and don't warn about
+  # indexing with tensors
+  options(tensorflow.one_based_extract = FALSE)
+  options(tensorflow.extract.warn_tensors_passed_asis = FALSE)
+
+  # default float type
+  options(greta_tf_float = "float64")
+}
