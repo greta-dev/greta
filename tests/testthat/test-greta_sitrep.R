410678cb97f4ed65f6db6cddee00732a0735276e
Nicholas Tierney <nicholas.tierney@gmail.com>
2022-01-31 17:27:23 +1000

    skip these tests on CI and rollback the dev use of mockery

--- tests/testthat/test-greta_sitrep.R
+++ tests/testthat/test-greta_sitrep.R
@@ -1,4 +1,6 @@
 test_that("greta_sitrep warns when have_python, _tf, or _tfp is FALSE", {
+  skip_on_ci()
+  skip_on_cran()
   mockery::stub(greta_sitrep, 'have_python', FALSE, 2)
   mockery::stub(greta_sitrep, 'have_tf', TRUE, 2)
   mockery::stub(greta_sitrep, 'have_tfp', TRUE, 2)
@@ -67,6 +69,8 @@ test_that("greta_sitrep warns when have_python, _tf, or _tfp is FALSE", {
 })
 
 test_that("greta_sitrep warns when different versions of python, tf, tfp", {
+  skip_on_ci()
+  skip_on_cran()
   mockery::stub(greta_sitrep, 'reticulate::py_version', "3.6", 2)
 
   expect_snapshot(
