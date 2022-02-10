7620669733621cb47349b3d325ae2804f167efc9
Nicholas Tierney <nicholas.tierney@gmail.com>
2022-02-10 14:11:07 +0800

    ensure full argument names are being passed

--- R/greta_install_python_deps.R
+++ R/greta_install_python_deps.R
@@ -21,8 +21,8 @@ greta_install_python_deps <- function(timeout) {
   install_python_modules <- new_install_process(
     callr_process = callr_conda_install,
     timeout = timeout,
-    stdout = stdout_file,
-    stderr = stderr_file,
+    stdout_file = stdout_file,
+    stderr_file = stderr_file,
     cli_start_msg = "Installing python modules into 'greta-env' conda \\
                      environment, this may take a few minutes",
     cli_end_msg = "Python modules installed!"
