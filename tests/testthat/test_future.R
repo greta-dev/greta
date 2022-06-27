1d1e7dcada5a7dfbb4af0b8f023e2ead94c4898c
Nicholas Tierney <nicholas.tierney@gmail.com>
2022-06-27 17:42:49 +0800

    add test for only one core

--- tests/testthat/test_future.R
+++ tests/testthat/test_future.R
@@ -1,3 +1,16 @@
+test_that("check_future_plan() works when only one core available", {
+  # temporarily set the envvar to have only 1 core available
+  withr::local_envvar("R_PARALLELLY_AVAILABLE_CORES" = "1")
+  op <- future::plan()
+  # put the future plan back as we found it
+  withr::defer(future::plan(op))
+  future::plan(future::multisession)
+
+  # one chain
+  expect_error(check_future_plan(), NA)
+
+})
+
 test_that("check_future_plan() works", {
   op <- future::plan()
   # put the future plan back as we found it
