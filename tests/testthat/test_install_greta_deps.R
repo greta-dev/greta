e000b40f3ffee1149d34980b9ef408cf03003028
Nicholas Tierney <nicholas.tierney@gmail.com>
2022-07-26 13:08:41 +0800

    various testthat snaps

--- tests/testthat/test_install_greta_deps.R
+++ tests/testthat/test_install_greta_deps.R
@@ -1,14 +1,13 @@
 test_that("install_greta_deps errors appropriately", {
   skip_if_not(check_tf_version())
   expect_snapshot_error(
-    object = install_greta_deps(timeout = 0.001),
-    regexp = "^Stopping as installation of"
+    install_greta_deps(timeout = 0.001)
   )
 })
 
-test_that("reinstall_greta_deps errors appropriately", {
-  skip_if_not(check_tf_version())
-  expect_snapshot_error(
-    object = reinstall_greta_deps(timeout = 0.001)
-  )
-})
+# test_that("reinstall_greta_deps errors appropriately", {
+#   skip_if_not(check_tf_version())
+#   expect_snapshot_error(
+#     reinstall_greta_deps(timeout = 0.001)
+#   )
+# })
