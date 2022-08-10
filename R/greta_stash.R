1ebaf3279ea2f7ef3f7a0be0566f4dcb5b03f4b6
njtierney <nicholas.tierney@gmail.com>
2022-08-10 11:25:39 +0800

    Small fix to docs in greta_stash

--- R/greta_stash.R
+++ R/greta_stash.R
@@ -10,11 +10,11 @@ greta_stash$conda_create_notes <- greta_note_msg
 greta_stash$conda_install_notes <- greta_note_msg
 greta_stash$tf_num_error <- greta_note_msg
 
-#' @title Retrieve python installation or error details
+#' @title Retrieve python installation or error details.
 #'
-#' These functions retrieve installation or error information output by python
-#'   when running `install_miniconda()`, `conda_create()`, `conda_install()`, or
-#'   when encountering a TensorFlow numerical problem.
+#' @description These functions retrieve installation or error information
+#'   output by python when running `install_miniconda()`, `conda_create()`,
+#'   `conda_install()`, or when encountering a TensorFlow numerical problem.
 #'
 #' @rdname stash-notes
 #' @export
