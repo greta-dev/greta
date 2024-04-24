14c9f84dea2aedb2a8d667c816cfa3e875e23606
njtierney <nicholas.tierney@gmail.com>
2024-04-24 16:00:09 +1000

    move failing cholesky tests out into separate test file rebase

---
+++ tests/testthat/test_distributions_cholesky.R
@@ -0,0 +1,26 @@
+test_that("Wishart can use a choleskied Sigma", {
+  skip_if_not(check_tf_version())
+
+  sig <- lkj_correlation(2, dim = 2)
+  w <- wishart(5, sig)
+  m <- model(w, precision = "double")
+  expect_ok(draws <- mcmc(m, warmup = 0, n_samples = 5, verbose = FALSE))
+})
+
+test_that("Cholesky factor of Wishart should be an upper triangular matrix", {
+  x <- wishart(df = 4, Sigma = diag(3))
+  chol_x <- chol(x)
+  calc_chol <- calculate(x, chol_x, nsim = 1)
+  expect_equal(dim(calc_chol$chol_x), c(1,3,3))
+  calc_chol_mat <- matrix(calc_chol$chol_x, nrow = 3, ncol = 3)
+  expect_equal(calc_chol_mat[lower.tri(calc_chol_mat)], c(0,0,0))
+})
+
+test_that("Cholesky factor of LJK_correlation should be an upper triangular matrix", {
+  x <- lkj_correlation(eta = 3, dimension = 3)
+  chol_x <- chol(x)
+  calc_chol <- calculate(x, chol_x, nsim = 1)
+  expect_equal(dim(calc_chol$chol_x), c(1,3,3))
+  calc_chol_mat <- matrix(calc_chol$chol_x, nrow = 3, ncol = 3)
+  expect_equal(calc_chol_mat[lower.tri(calc_chol_mat)], c(0,0,0))
+})
