f85f489019a6c45f03c505fdb6063a18ee3f145c
njtierney <nicholas.tierney@gmail.com>
2024-07-30 15:38:00 +1000

    set seeds for reproducibility

--- tests/testthat/test_distributions_cholesky.R
+++ tests/testthat/test_distributions_cholesky.R
@@ -6,6 +6,7 @@ test_that("Wishart can use a choleskied Sigma", {
   sig <- lkj_correlation(2, dim = 2)
   w <- wishart(5, sig)
   m <- model(w, precision = "double")
+  tensorflow::set_random_seed(2024-07-30-1520)
   expect_ok(draws <- mcmc(m, warmup = 0, n_samples = 5, verbose = FALSE))
 })
 
@@ -16,7 +17,7 @@ test_that("Cholesky factor of Wishart should be a lower triangular matrix", {
   x <- wishart(df = 4, Sigma = diag(3))
   chol_x <- chol(x)
   expect_snapshot(
-    calculate(chol_x, nsim = 1)
+    calculate(chol_x, nsim = 1, seed = 2024-07-30-1431)
   )
   calc_x <- calculate(x, nsim = 1)
   calc_chol <- calculate(chol_x, nsim = 1)
@@ -28,7 +29,7 @@ test_that("Cholesky factor of Wishart should be a lower triangular matrix", {
   x <- wishart(df = 4, Sigma = diag(3))
   chol_x <- chol(x)
   expect_snapshot(
-    (calc_chol <- calculate(x, chol_x, nsim = 1))
+    (calc_chol <- calculate(x, chol_x, nsim = 1, seed = 2024-07-30-1431))
   )
   expect_square(calc_chol$chol_x)
   expect_lower_tri(calc_chol$chol_x)
@@ -41,10 +42,10 @@ test_that("Cholesky factor of LJK_correlation should be a lower triangular matri
   x <- lkj_correlation(eta = 3, dimension = 3)
   chol_x <- chol(x)
   expect_snapshot(
-    calculate(chol_x, nsim = 1)
+    calculate(chol_x, nsim = 1, seed = 2024-07-30-1431)
   )
-  calc_x <- calculate(x, nsim = 1)
-  calc_chol <- calculate(chol_x, nsim = 1)
+  calc_x <- calculate(x, nsim = 1, seed = 2024-07-30-1431)
+  calc_chol <- calculate(chol_x, nsim = 1, seed = 2024-07-30-1431)
 
   expect_lower_tri(calc_chol$chol_x)
   expect_square(calc_chol$chol_x)
@@ -53,7 +54,7 @@ test_that("Cholesky factor of LJK_correlation should be a lower triangular matri
   x <- lkj_correlation(eta = 3, dimension = 3)
   chol_x <- chol(x)
   expect_snapshot(
-    (calc_chol <- calculate(x, chol_x, nsim = 1))
+    (calc_chol <- calculate(x, chol_x, nsim = 1, seed = 2024-07-30-1431))
   )
   expect_square(calc_chol$chol_x)
   expect_lower_tri(calc_chol$chol_x)
@@ -63,9 +64,10 @@ test_that("Post-MCMC, Wishart distribution stays symmetric, chol remains lower t
 # From https://github.com/greta-dev/greta/issues/585
   x <- wishart(df = 4, Sigma = diag(3))
   m <- model(x)
+  tensorflow::set_random_seed(2024-07-30-1431)
   draws <- mcmc(m, warmup = 1, n_samples = 1)
 
-  calcs <- calculate(x, chol(x), nsim = 1)
+  calcs <- calculate(x, chol(x), nsim = 1, seed = 2024-07-30-1431)
   # ensure that the symmetric matrix is still symmetric
   expect_snapshot(
     calcs
@@ -82,9 +84,10 @@ test_that("Post-MCMC, LKJ distribution stays symmetric, chol remains lower tri",
   # From https://github.com/greta-dev/greta/issues/585
   x <- lkj_correlation(eta = 3, dimension = 3)
   m <- model(x)
+  tensorflow::set_random_seed(2024-07-30-1431)
   draws <- mcmc(m, warmup = 1, n_samples = 1)
 
-  calcs <- calculate(x, chol(x), nsim = 1)
+  calcs <- calculate(x, chol(x), nsim = 1, seed = 2024-07-30-1431)
   # ensure that the symmetric matrix is still symmetric
   expect_snapshot(
     calcs
