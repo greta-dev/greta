bab7d5cb7f1e6efda28f156b593783d5ed44b708
njtierney <nicholas.tierney@gmail.com>
2024-07-30 14:55:06 +1000

    remove erraneous x argument from unknowns

--- R/unknowns_class.R
+++ R/unknowns_class.R
@@ -58,7 +58,7 @@ print.unknowns <- function(x, ..., n = 10) {
 }
 
 # create an unknowns array from some dimensions
-unknowns <- function(x, dims = c(1, 1), data = as.numeric(NA)) {
+unknowns <- function(dims = c(1, 1), data = as.numeric(NA)) {
   x <- array(data = data, dim = dims)
   as.unknowns(x)
 }
