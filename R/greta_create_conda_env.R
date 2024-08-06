7b9eb5c1816ab4d9ad5f84b0b70a8942e8126dd4
njtierney <nicholas.tierney@gmail.com>
2024-08-06 11:38:53 +1000

    * Added option to restart R + run `library(greta)` after installation (#523) * Added installation deps object, `greta_python_deps()` to help simplify specifying package versions (#664) * removed `method` and `conda` arguments from `install_greta_deps()` as they   were not used. * removed `manual` option in `install_greta_deps()` * added default 5 minute timer to installation processes

--- R/greta_create_conda_env.R
+++ R/greta_create_conda_env.R
@@ -1,17 +1,17 @@
-greta_create_conda_env <- function(timeout) {
-
-  options(warning.length = 2000)
+greta_create_conda_env <- function(timeout = 5,
+                                   python_deps = greta_python_deps()) {
 
   stdout_file <- create_temp_file("out-greta-conda")
   stderr_file <- create_temp_file("err-greta-conda")
 
   callr_conda_create <- callr::r_process_options(
-    func = function() {
+    func = function(python_version) {
       reticulate::conda_create(
         envname = "greta-env-tf2",
-        python_version = "3.10"
+        python_version = python_version
       )
     },
+    args = list(python_version = python_deps$python_version),
     stdout = stdout_file,
     stderr = stderr_file
   )
@@ -21,8 +21,10 @@ greta_create_conda_env <- function(timeout) {
     stdout_file = stdout_file,
     stderr_file = stderr_file,
     timeout = timeout,
-    cli_start_msg = "Creating 'greta-env-tf2' conda environment using python \\
-                      v3.8, this may take a minute",
+    cli_start_msg = glue::glue(
+    "Creating 'greta-env-tf2' conda environment using python \\
+    v{python_deps$python_version}, this may take a minute"
+    ),
     cli_end_msg = "greta-env-tf2 environment created!"
   )
 
