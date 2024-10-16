1bf0946fad37313455d79631d20fcaed9d36dba8
njtierney <nicholas.tierney@gmail.com>
2024-10-16 13:35:12 +1100

    * add argument names in test_posteriors_wishart.R * update test_seed.R to use a slightly different machine precision via expect_equal rather than expect_idential

--- tests/testthat/test_posteriors_wishart.R
+++ tests/testthat/test_posteriors_wishart.R
@@ -6,11 +6,15 @@ test_that("samplers are unbiased for Wishart", {
   skip_if_not_release()
 
   sigma <- matrix(
-    c(1.2, 0.7, 0.7, 2.3),
-    2, 2
+    data = c(1.2, 0.7, 0.7, 2.3),
+    nrow = 2,
+    ncol = 2
   )
+
   df <- 4
+
   x <- wishart(df, sigma)[1, 2]
+
   iid <- function(n) {
     rWishart(n, df, sigma)[1, 2, ]
   }
